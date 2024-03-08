cb249d00fb26580eb8a0d81a0fedd5050a8e1c54
Ariana <arianahejazyan@gmail.com>
2024-03-08 14:59:21 +0330

    Array implementation

--- code/DS/Array/main.h
+++ code/DS/Array/main.h
@@ -1,3 +1,4 @@
+#include <initializer_list>
 #include <stddef.h>
 
 template<typename T>
@@ -5,25 +6,45 @@ class Array
 {
     private:
 
-        T* arr;
-        size_t size_;
-        size_t capacity_;
+        T* m_elements;
+        size_t m_size;
+        size_t m_capacity;
+
+        void checkIndex(size_t index) const;
+
+        void checkUnderFlow() const;
+
+        void growCapacity();
+
+        void clear();
 
     public:
 
         Array();
 
-        Array(size_t capacity_);
+        Array(size_t capacity);
+
+        Array(std::initializer_list<T> list);
 
         ~Array();
-        
-        T& operator[](size_t index) const;
 
-        void push_back(const T& element);
-
-        void pop_back();
+        Array& operator=(std::initializer_list<T> list);
 
         size_t capacity() const;
 
         size_t size() const;
+
+        T& operator[](size_t index) const;
+
+        void push_back(const T& element);
+
+        T& pop_back();
+
+        void insert(const T& element, size_t index);
+
+        void erase(size_t index);
+
+        T* begin() const;
+
+        T* end() const;
 };
