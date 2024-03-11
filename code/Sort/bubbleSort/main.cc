7682e3b1a41f1a4b410fec76e89abcd4e60f8e2f
Ariana <arianahejazyan@gmail.com>
2024-03-11 12:25:57 +0330

    Bubble sort implementation

---
+++ code/Sort/bubbleSort/main.cc
@@ -0,0 +1,12 @@
+#include "../../DS/Array/main.h"
+#include "../../utils/utils.h"
+
+void bubbleSort(Array<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
