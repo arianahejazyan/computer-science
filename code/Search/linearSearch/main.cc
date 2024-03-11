7fbb1c6062058afffaa8f5137e693f6a006920da
Ariana <arianahejazyan@gmail.com>
2024-03-11 15:19:13 +0330

    Linear search implementation

--- code/Search/linearSearch/main.cc
+++ code/Search/linearSearch/main.cc
@@ -1,20 +1,9 @@
 #include "../../DS/Array/main.h"
 
 template<typename T>
-int linearSearchIterative(const Array<T>& arr, const T& key) {
+int linearSearch(const Array<T>& arr, const T& key) {
     int i = 0;
     for (const T& element: arr) {
         if (element == key) return i; i++;
     } return -1;
-}
-
-template<typename T>
-int linearSearchRecursive(const Array<T>& arr, const T& key, int idx=0) {
-    if (idx >= arr.size())
-        return -1;
-
-    if (arr[idx] == key)
-        return idx;
-    
-    return linearSearchRecursive(arr, key, idx + 1);
 }
